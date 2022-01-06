f7bc8441042a8baa4e48581d09a12dfaee32ec5a
Anivie <88354708+Anivie@users.noreply.github.com>
2022-01-06 20:04:48 +0800

    Delete Lyric-Maker directory

--- Lyric-Maker/src/main/kotlin/ink/bluecloud/lyricmaker/Main.kt
+++
@@ -1,8 +0,0 @@
-package ink.bluecloud.lyricmaker
-
-import ink.bluecloud.lyricmaker.MainView.MainView
-import javafx.application.Application
-
-fun main(args: Array<String>) {
-    Application.launch(MainView::class.java,*args)
-}
